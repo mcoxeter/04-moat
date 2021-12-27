468d246ae6b961728371755f0a60fee11e094838
Michael Coxeter <mcoxeter@gamil.com>
2021-12-27 11:39:21 +0100

    Improved cagr to handle negative numbers

--- src/index.ts
+++ src/index.ts
@@ -309,23 +309,22 @@ function analyseCAGR(
   };
 }
 
-// function cagr(start: number, end: number) {
-//   // CAGR = Compound Annual Growth Rate
-//   // https://www.investopedia.com/terms/c/cagr.asp
-//   return Math.round((Math.pow(end / start, 0.5) - 1) * 100);
-// };
-
 function cagr(start: number, end: number, number: number) {
+  let offsetStart = start;
+  let offsetEnd = end;
+  if (start < 0) {
+    offsetStart += Math.abs(start);
+    offsetEnd += Math.abs(start);
+  }
+
+  if (end < 0) {
+    offsetStart += Math.abs(end);
+    offsetEnd += Math.abs(end);
+  }
+
   // CAGR = Compound Annual Growth Rate
   // https://www.investopedia.com/terms/c/cagr.asp
-  // http://fortmarinus.com/blog/1214/
-
-  const step1 = end - start + Math.abs(start);
-  const step2 = step1 / Math.abs(start);
-  const step3 = Math.pow(step2, 1 / number);
-  const step4 = (step3 - 1) * 100;
-
-  return Math.round(step4);
+  return Math.round((Math.pow(offsetEnd / offsetStart, 1 / number) - 1) * 100);
 }
 
 app();
